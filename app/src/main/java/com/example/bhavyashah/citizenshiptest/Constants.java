0cb0b085770018dd4f5be91908e06db8ca183d13
Bhavya Shah <bshah27@its.jnj.com>
2017-11-09 23:15:13 -0500

    Added first 71 questions to constants file

---
+++ app/src/main/java/com/example/bhavyashah/citizenshiptest/Constants.java
@@ -0,0 +1,104 @@
+package com.example.bhavyashah.citizenshiptest;
+
+public class Constants {
+
+    /**
+     * ---------------------    AMERICAN GOVERNMENT    -------------------
+     */
+
+    /**
+     * Principles of American Democracy
+     */
+    public String QUESTION_1  = "What is the supreme law of the land";
+    public String QUESTION_2  = "What does the Constitution do?";
+    public String QUESTION_3  = "What are the first 3 words of the Constitution?";
+    public String QUESTION_4  = "What is an amendment?";
+    public String QUESTION_5  = "What do we call the first 10 amendments to the Constitution?";
+    public String QUESTION_6  = "What is one right of freedom from the First Amendment?";
+    public String QUESTION_7  = "How many amendments does the Constitution have?";
+    public String QUESTION_8  = "What did the Declaration of Independence do?";
+    public String QUESTION_9  = "What are two rights in the Declaration of Independence?";
+    public String QUESTION_10 = "What is freedom of religion?";
+    public String QUESTION_11 = "What is the economic system in the United States?";
+    public String QUESTION_12 = "What is the \"rule of law\"?";
+
+    /**
+     * System of Government
+     */
+    public String QUESTION_13 = "Name one branch of government.";
+    public String QUESTION_14 = "What stops one branch of government from becoming too powerful?";
+    public String QUESTION_15 = "Who is in charge of the executive branch?";
+    public String QUESTION_16 = "Who makes federal laws?";
+    public String QUESTION_17 = "What are the two parts of the U.S. Congress?";
+    public String QUESTION_18 = "How many US senators are there?";
+    public String QUESTION_19 = "We elect a US senator for how long?";
+    public String QUESTION_20 = "Who is one of your state's US senators";
+    public String QUESTION_21 = "The House of Representatives has how many voting members?";
+    public String QUESTION_22 = "We elect a US representative for how many years?";
+    public String QUESTION_23 = "Name your US Representative?";
+    public String QUESTION_24 = "What does a US Senator represent?";
+    public String QUESTION_25 = "Why do some states have more Representatives than other states?";
+    public String QUESTION_26 = "We elect a President for how many years?";
+    public String QUESTION_27 = "In what month do we vote for the President?";
+    public String QUESTION_28 = "What is the name of the President of the United States now?";
+    public String QUESTION_29 = "What is the name of the Vice President of the United States now?";
+    public String QUESTION_30 = "If the President can no longer serve, who becomes president?";
+    public String QUESTION_31 = "If both the president and vice president can no longer serve, who becomes president?";
+    public String QUESTION_32 = "Who is the Commander in Chief of the military?";
+    public String QUESTION_33 = "Who signs bills to become law?";
+    public String QUESTION_34 = "Who vetoes bills?";
+    public String QUESTION_35 = "What does the President's Cabinet do?";
+    public String QUESTION_36 = "What are two Cabinet-level positions?";
+    public String QUESTION_37 = "What does the judicial branch do?";
+    public String QUESTION_38 = "What is the highest court in the United States?";
+    public String QUESTION_39 = "How many justices are on the Supreme Court?";
+    public String QUESTION_40 = "Who is the Chief Justice of the United States?";
+    public String QUESTION_41 = "The Constitution gives some powers to the federal government. What is one of these powers?";
+    public String QUESTION_42 = "The Constitution gives some powers to the states. What is one of these powers?";
+    public String QUESTION_43 = "Who is the Governor of your state now?";
+    public String QUESTION_44 = "What is the capital of your state?";
+    public String QUESTION_45 = "What are the two major political parties in the United States";
+    public String QUESTION_46 = "What is the political party of the President now?";
+    public String QUESTION_47 = "What is the name of the Speaker of the House of Representatives now?";
+
+    /**
+     * Rights and Responsibilities
+     */
+    public String QUESTION_48 = "There are 4 amendments about who can vote. Describe one of them.";
+    public String QUESTION_49 = "What is one responsibility that is only for US citizens?";
+    public String QUESTION_50 = "Name one right only for US citizens.";
+    public String QUESTION_51 = "What are 2 rights of everyone living in the US?";
+    public String QUESTION_52 = "What do we show loyalty to when we say the Pledge of Allegiance?";
+    public String QUESTION_53 = "What is one promise you make when you become a US citizen";
+    public String QUESTION_54 = "How old do citizens have to be to vote for President?";
+    public String QUESTION_55 = "What are 2 ways that Americans can participate in their democracy?";
+    public String QUESTION_56 = "When is the last day you can send in federal income tax forms?";
+    public String QUESTION_57 = "When must all men register for the Selective Service?";
+
+    /**
+     * ---------------------    AMERICAN HISTORY    -------------------
+     */
+
+    /**
+     * Colonial Period and Independence
+     */
+
+    public String QUESTION_58 = "What is one reason colonists came to America?";
+    public String QUESTION_59 = "Who lived in America before the Europeans arrived?";
+    public String QUESTION_60 = "What group of people was taken to America and sold to slaves?";
+    public String QUESTION_61 = "Why did the colonists fight the British?";
+    public String QUESTION_62 = "Who wrote the Declaration of Independence?";
+    public String QUESTION_63 = "When was the Declaration of Independence adopted?";
+    public String QUESTION_64 = "There are 13 original states. Name 3.";
+    public String QUESTION_65 = "What happened at the Constitutional Convention?";
+    public String QUESTION_66 = "When was the Constitution written?";
+    public String QUESTION_67 = "The Federalist Papers supported the passage of the US Constitution. Name one of the writers.";
+    public String QUESTION_68 = "What is one thing Benjamin Franklin is famous for?";
+    public String QUESTION_69 = "Who is the \"Father of Our Country\"?";
+    public String QUESTION_70 = "Who was the first President?";
+
+    /**
+     * 1800s
+     */
+    public String QUESTION_71 = "What territory did the US buy from France in 1803?";
+}
